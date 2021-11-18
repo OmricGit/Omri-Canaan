2fb38fb49b391755af8e8db7737456438f0ed248
OmricGit <omrican@post.bgu.ac.il>
2021-11-18 19:04:06 +0200

    exercise5

---
+++ animation1.js
@@ -0,0 +1,10 @@
+function thankyou() {
+    document.getElementById("Button").innerHTML = "Nice to see you"; 
+}
+ 
+function changepic() {
+    document.getElementById("HelAnimation").src = "OmriPic.jpeg";
+    console.log("Omri Canaan");
+
+}
+
